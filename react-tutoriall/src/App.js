be6267da1ce5561097e7d6f3c1f6ebe6b147b32e
Rohit9Mankar <rohitmankar0079@gmail.com>
2023-01-29 15:55:19 +0530

    expense items with for loop

--- react-tutoriall/src/App.js
+++ react-tutoriall/src/App.js
@@ -1,10 +1,53 @@
-import ExpenseItem from "./components/ExpenseItem";  
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+  const expenses = [
+    {
+      id: 'e1',
+      date: new Date(2023, 1, 28),
+      title: "Car Insurance",
+      amount: 234,
+      location: "Nagpur"
+    },
+    {
+      id: 'e2',
+      date: new Date(2022, 1, 28),
+      title: "Surgery",
+      amount: 23445,
+      location: "Nagpur"
+    },
+    {
+      id: 'e3',
+      date: new Date(2023, 1, 21),
+      title: "Goa trip",
+      amount: 10000,
+      location: "Goa"
+    },
+    {
+      id: 'e4',
+      date: new Date(2022, 1, 20),
+      title: "Bike",
+      amount: 19087,
+      location: "Nagpur"
+    }
+  ]
+
   return (
+
     <div>
-      <h2>Let's get started!</h2>   
-      <ExpenseItem></ExpenseItem>
+      <h1>Expense Items</h1>
+      {expenses.map(expense => (
+
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          location={expense.location}>
+        </ExpenseItem>
+
+      )
+      )}
     </div>
   );
 }
--- react-tutoriall/src/components/ExpenseItem.js
+++ react-tutoriall/src/components/ExpenseItem.js
@@ -1,13 +1,19 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+ 
   return (
-    <div>
-      <h2>Expense Items</h2>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div>{props.location}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
 
-      <p>Food Rs 10</p>
-
-      <p>Petrol Rs 100</p>
-      <p>Movies Rs 200</p>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
     </div>
+
+
   );
 }
 export default ExpenseItem;
