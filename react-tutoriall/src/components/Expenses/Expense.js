e704d3011c03fc4ad246a1c07cb6bf707350fe71
Rohit9Mankar <rohitmankar0079@gmail.com>
2023-02-08 16:33:25 +0530

    conditional rendering for zero or one expenses

--- react-tutoriall/src/components/Expenses/Expense.js
+++ react-tutoriall/src/components/Expenses/Expense.js
@@ -16,18 +16,29 @@ const Expenses = (props) => {
     return expenses.date.getFullYear().toString()===filteredYear;
   });
 
+let expenseContent=filteredExpenses.map((item) => (
+  <ExpenseItem 
+  
+  key={item.id}
+  title={item.title}
+  amount={item.amount}
+  date={item.date}></ExpenseItem>
+ ));
+ let message="";
+ if(filteredExpenses.length===0){
+  expenseContent=<p>No expenses here</p>
+ }
+ if(filteredExpenses.length===1){
+  message=<p>Only single Expense here. Please add more...</p>
+ }
+
   return (
     
     <Card className="expenses">
       <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-   {filteredExpenses.map((item) => (
-    <ExpenseItem 
-    
-    key={item.id}
-    title={item.title}
-    amount={item.amount}
-    date={item.date}></ExpenseItem>
-   ))}
+      {expenseContent}
+      {message}
+   
    
     </Card>
   );
